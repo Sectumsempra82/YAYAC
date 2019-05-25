139ebcf7600c22d9158c604fa7d0fce5aa6f1ab9
Sectumsempra82 <sectumsempra82@gmail.com>
2019-05-25 19:39:09 +0200

    unified express and react
    Creation of proper npm scripts
    Redesigned fodler structure
    Removed some useless code
    Fixed npm run watch and npm start

--- src/app.js
+++ src/app.js
@@ -1,23 +1,16 @@
 import express from 'express';
 import path from 'path';
-// import cookieParser from 'cookie-parser';
-// import logger from 'morgan';
-
-// import indexRouter from './routes/index';
-
-
-
 
 const app = express();
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// An api endpoint that returns a short list of items
+// An api endpoint that returns a list of academies
 app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
+    var list = ["academy1", "academy2", "academy3"];
     res.json(list);
-    console.log('Sent list of items');
+    console.log('Sent list of yoda academies');
 });
 
 // Routes everything else to our React app
@@ -27,7 +20,7 @@ app.get('*', (req,res) =>{
 
 const port = process.env.PORT || 5000;
 app.listen(port);
-
+ 
 console.log('App is listening on port ' + port);
 
-export default app;
+
