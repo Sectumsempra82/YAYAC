63b10c31b4026ecdd3bdba410231dc96f1d4cdfe
Sectumsempra82 <sectumsempra82@gmail.com>
2019-05-25 18:52:42 +0200

    added basic api and react client

--- src/app.js
+++ src/app.js
@@ -1,18 +1,33 @@
 import express from 'express';
 import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+// import cookieParser from 'cookie-parser';
+// import logger from 'morgan';
 
-import indexRouter from './routes/index';
+// import indexRouter from './routes/index';
+
+
+
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use('/', indexRouter);
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
+
+// Routes everything else to our React app
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log('App is listening on port ' + port);
 
 export default app;
